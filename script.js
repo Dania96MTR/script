81f0c6478e45173f1241d17ba40dd4e36592c5c6
Dania96MTR <144219920+Dania96MTR@users.noreply.github.com>
2023-09-12 16:56:34 +0300

    variable

---
+++ script.js
@@ -0,0 +1,9 @@
+let information = [ "firstname" , "lastname" , "age" ]
+information[0] = document.getElementsByClassName("first")
+information[1] = document.getElementsByClassName("second")
+information[2] = document.getElementsByClassName("third")
+let allinfo = document.querySelector("div")
+allinfo.classList.add("look")
+console.log(information[0])
+console.log(information[1])
+console.log(information[2])
